496563a629f3ef7162b136adef250459d9b0ce95
Gaurav Sood <gsood07@gmail.com>
2017-04-05 09:38:03 -0400

    modest update of goji

---
+++ R/as_num.R
@@ -0,0 +1,17 @@
+#' Gently coerce to numeric: convert all chars to NA
+#' 
+#' 
+#' @param x vector
+#' @return vector
+#' 
+#' @export
+#' @examples
+#' x <- c("a", 1, 2); as_num(x)
+#' 
+#' # Alternate way: 
+#' x <- c("a", 1, 2); class(x) <- "numeric"
+#' 
+
+as_num <- function(x){
+	as.numeric(ifelse(grepl("[a-z]", x), NA, x))
+}
---
+++ R/goji.R
@@ -0,0 +1,12 @@
+#' @title goji: Miscellaneous utility functions by G!
+#' 
+#' @name goji-package
+#' @aliases goji
+#'
+#' @description Recommended for co-authors of G and those trying to use his scripts.
+#' 
+#' @importFrom stats cov var weighted.mean
+#' @docType package
+#' @author Gaurav Sood
+NULL
+
