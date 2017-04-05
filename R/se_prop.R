6c989f5e819e25a79097e81e1388cd1ed43b2602
Gaurav Sood <gsood07@gmail.com>
2017-04-05 10:24:41 -0400

    se prop

---
+++ R/se_prop.R
@@ -0,0 +1,17 @@
+#' SE for the proportion
+#'
+#' Takes p and n
+#' 
+#' @param p proportion
+#' @param n size of the vector
+#' 
+#' @return vector
+#' 
+#' @export
+#' @examples
+#' se_prop(.50, 100)
+#' 
+
+se_prop <- function(p, n) {
+	sqrt(p*(1 - p)/n)
+}
