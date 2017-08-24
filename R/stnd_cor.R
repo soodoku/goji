ca62dcc18b239af07b583f83dfa46998a281aa08
Gaurav Sood <gsood07@gmail.com>
2017-08-24 18:10:05 -0400

    default item to NULL, and apparently T now has to be TRUE

--- R/stnd_cor.R
+++ R/stnd_cor.R
@@ -1,17 +1,17 @@
 #' Standard Correction for Guessing For Multiple Choice Items With DKs
 #' 
-#' @param item response to the knowledge item. We assume that the domain of values
+#' @param item vector; Response to the knowledge item. We assume that the domain of values
 #' that the vector can take is: 0 (incorrect answer), 1 (correct answer), 'dk' (Don't Know),
 #' and NAs. We assume missing values are MCAR. Missing values in knowledge items are best
 #' treated as confessions of ignorance. And if you agree with the point, pass the 
 #' values through \code{nona} to convert NAs to 0.
-#' @param n_options total number of options; numeric; default is 4
+#' @param n_options numeric; Total number of options. Default is 4.
 #' 
 #' @export
 #' @examples
-#' x <- c(NA, 1, 0, 1, 0, 0); mean(x, na.rm = T); stnd_cor(x, 4)
+#' x <- c(NA, 1, 0, 1, 0, 0); mean(x, na.rm = TRUE); stnd_cor(x, 4)
 
-stnd_cor <- function(item, n_options = 4) {
+stnd_cor <- function(item = NULL, n_options = 4) {
 
   total_correct <- sum(item == 1, na.rm = TRUE)
   total_incorrect <- sum(item == 0, na.rm = TRUE)
